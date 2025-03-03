149b1a7112fb85180948395d24a7cefbe203ca4f
Sominda Gamage <sominda94@gmail.com>
2025-03-03 12:36:12 -0500

    Revert "Implementing secure APIs with JWTs"
    This reverts commit 1b532115580a2b9a67318c0c6885afbf7d950163.

--- payment-manager/src/main/java/com/sominda/sample/payment_manager/controller/PaymentController.java
+++ payment-manager/src/main/java/com/sominda/sample/payment_manager/controller/PaymentController.java
@@ -1,17 +1,11 @@
 package com.sominda.sample.payment_manager.controller;
 
-import com.sominda.sample.payment_manager.exception.PaymentManagerServerException;
 import com.sominda.sample.payment_manager.model.Payment;
 import com.sominda.sample.payment_manager.model.api.PaymentInitiatorResponse;
 import com.sominda.sample.payment_manager.model.api.PaymentRequest;
 import com.sominda.sample.payment_manager.service.PaymentManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +15,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/payments")
@@ -59,35 +52,5 @@ public class PaymentController {
     public List<Payment> getPaymentsByStatus(@PathVariable String status) {
 
         return paymentManagerService.getPaymentsByStatus(status);
-    }
-
-    @GetMapping("/authdata")
-    @ResponseStatus(HttpStatus.OK)
-    public void getAuthentication() {
-
-        // Print authenticated user details.
-        printAuthenticatedUser();
-    }
-
-    private void printAuthenticatedUser() {
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null) {
-            throw new PaymentManagerServerException("02", "Unable to find authentication data", "Server failed to " +
-                    "find user information for the authenticated user");
-        }
-        Object principal = authentication.getPrincipal();
-        if (principal == null) {
-            throw new PaymentManagerServerException("03", "Unable to find authenticated principal", "Server " +
-                    "failed to find user information for the authenticated user");
-        }
-        // Since this is a JWT, we can map it to jwt object type.
-        Map<String, Object> claims = ((Jwt) principal).getClaims();
-        if (CollectionUtils.isEmpty(claims)) {
-            throw new PaymentManagerServerException("04", "Unable to authenticated user claims", "Server obtain " +
-                    "authenticated user claims");
-        }
-        System.out.printf("Authenticated user: %s, %s, %s, %s%n", claims.get("sid"),claims.get(
-                "username"), claims.get("given_name"),claims.get("email"));
     }
 }
