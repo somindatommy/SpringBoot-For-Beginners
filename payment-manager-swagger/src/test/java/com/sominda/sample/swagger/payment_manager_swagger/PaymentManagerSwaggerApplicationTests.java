9505ab9b75e86b7755a17faa87563a6f1f5dbc32
Sominda Gamage <sominda94@gmail.com>
2025-03-07 00:34:56 -0500

    Implement integration with swagger API definitions

---
+++ payment-manager-swagger/src/test/java/com/sominda/sample/swagger/payment_manager_swagger/PaymentManagerSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sominda.sample.swagger.payment_manager_swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentManagerSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- payment-manager/src/main/java/com/sominda/sample/payment_manager/controller/PaymentController.java
+++ payment-manager/src/main/java/com/sominda/sample/payment_manager/controller/PaymentController.java
@@ -8,7 +8,6 @@ import com.sominda.sample.payment_manager.service.PaymentManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.util.CollectionUtils;
