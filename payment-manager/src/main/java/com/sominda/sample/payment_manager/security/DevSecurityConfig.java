9a1caaad4b10738c58544d6b3a1bf230ff122724
Sominda Gamage <sominda94@gmail.com>
2025-03-16 19:13:09 -0400

    Implementing dockerfile

--- payment-manager/src/main/java/com/sominda/sample/payment_manager/security/DevSecurityConfig.java
+++ payment-manager/src/main/java/com/sominda/sample/payment_manager/security/DevSecurityConfig.java
@@ -3,6 +3,7 @@ package com.sominda.sample.payment_manager.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,8 +17,10 @@ public class DevSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(authr -> {
+            authr.requestMatchers(HttpMethod.POST, "/api/v1/payments" ).permitAll();
             authr.anyRequest().permitAll();
-        });
+        })
+        .csrf(csrf -> csrf.disable());
         return http.build();
     }
 }
